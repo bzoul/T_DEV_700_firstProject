8125e461c7f3e974c8d9a4d69bebd553444cadce
basile <basile.rolland31@gmail.com>
2020-11-19 16:45:41 +0100

    login interface

---
+++ src/component/log.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import {Text, TextInput, View ,Button, StyleSheet } from 'react-native'
+
+const logIn = () => {
+    const [text, setText] = useState('');
+    return (
+      <View style={styles.main_container}>
+        <TextInput
+          style={styles.input_text}
+          placeholder="User Name"
+          
+          defaultValue={text}
+        />
+        <TextInput
+          style={styles.input_text}
+          placeholder="Password"
+          
+          defaultValue={text}
+        />
+        <Button title="LOG IN" styles= {styles.button_log}/>
+        <Text style={styles.register}> create your account </Text>
+        <Button style= {styles.button_log} title="register" />
+      </View>
+       );
+    }
+
+const styles = StyleSheet.create ({
+    main_container: {
+        height: 100,
+        marginTop: 150,
+        marginLeft: 30,
+        marginRight: 30
+    },
+    input_text: {
+        flex: 2
+    },
+    button_log: {
+        height: 20,
+        width: 50
+    },
+    register: {
+      textAlign:'center'
+    }
+})
+export default logIn;
--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import {Text, TextInput, View} from 'react-native'
+import log from '../component/log'
 
 const Login = () => {
 
   return (<></>)
 }
+
 
 export default Login
