ede6a2ebaf81ccd4772877e8fec8314b74b228b4
Clément <clement.lacroix@epitech.eu>
2020-12-10 16:51:56 +0100

    [+] NFC Reader implementation

--- src/views/NFC.jsx
+++ src/views/NFC.jsx
@@ -1,8 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import NfcCardReader from 'react-native-nfc-card-reader';
+import { Text, Button } from 'react-native'
 
 const NFCView = () => {
+
+  const [card, setCard] = useState({})
+
+  const startScan = async () => {
+    await NfcCardReader.startNfc((cardDetails) => {
+      console.log(cardDetails)
+       setCard(cardDetails);
+     })
+  }
+
+  useEffect(() => {
+    startScan();
+  }, [])
+
   return (
-    <></>
+    <>
+      <Text>{ card.cardType }</Text>
+      <Text>{ card.expiryDate }</Text>
+      <Text>{ card.cardNumber }</Text>
+    </>
   )
 }
 
