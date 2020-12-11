7c3ce95bfdd19147184f9718070574cb2013b9ef
bzoul <basile.rolland31@gmail.com>
2020-12-11 17:11:49 +0100

    fiew css change

--- src/components/LoginComponent.jsx
+++ src/components/LoginComponent.jsx
@@ -3,6 +3,11 @@ import {Text, TextInput, View ,Button, StyleSheet } from 'react-native'
 
 const LoginComponent = () => {
     const [text, setText] = useState('');
+
+    const goTo = (route) => {
+      Actions.push(route);
+    }
+
     return (
       <View style={styles.main_container}>
         <TextInput
@@ -15,22 +20,24 @@ const LoginComponent = () => {
           placeholder="Password"
           defaultValue={text}
         />
-        <Button title="LOG IN" styles= {styles.button_log}/>
+        <Button title="LOG IN" styles= {styles.button_log} onPress={() => goTo('register')}/>
         <Text style={styles.register}> create your account </Text>
         <Button style= {styles.button_log} title="register" />
       </View>
     );
 }
 
+
 const styles = StyleSheet.create ({
     main_container: {
         height: 100,
-        marginTop: 150,
+        marginTop: 250,
         marginLeft: 30,
         marginRight: 30
     },
     input_text: {
-        flex: 2,
+        height: 30,
+        fontSize: 20
     },
     button_log: {
         height: 20,
--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 import {Text, TextInput, View ,Button, StyleSheet } from 'react-native'
 
 const Register = () => {
+  
+  const goTo = (route) => {
+    Actions.push(route);
+  }
     return (
       <View style={styles.main_container}>
         <TextInput
@@ -16,7 +20,7 @@ const Register = () => {
           style={styles.input_text}
           placeholder="Password"
         />
-        <Button style= {styles.button_log} title="register" />
+        <Button style= {styles.button_log} title="register" onPress={() => goTo('login')}/>
       </View>
        );
     }
@@ -24,13 +28,15 @@ const Register = () => {
 const styles = StyleSheet.create ({
     main_container: {
         height: 100,
-        marginTop: 150,
+        marginTop: 250,
         marginLeft: 30,
         marginRight: 30,
         alignContent:"center"
     },
     input_text: {
-        flex: 2
+        height:50,
+        fontSize: 20,
+        margin:2
     },
     button_log: {
         height: 20,
