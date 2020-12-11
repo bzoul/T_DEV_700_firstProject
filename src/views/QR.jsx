573e17ab0a9c4d89d78fc33deb08111429910bdb
Clément <clement.lacroix@epitech.eu>
2020-12-11 13:58:19 +0100

    [+] QR Code implementation

--- src/views/QR.jsx
+++ src/views/QR.jsx
@@ -1,9 +1,50 @@
+'use strict';
+
 import React from 'react';
 
-const QRView = () => {
-  return (
-    <></>
-  )
+import {
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  Linking
+} from 'react-native';
+
+import QRCodeScanner from 'react-native-qrcode-scanner';
+import { RNCamera } from 'react-native-camera';
+
+const QrView = () => {
+  const onSuccess = (e) => {
+    Linking.openURL(e.data).catch(err =>
+      console.error('An error occured', err)
+    );
+  };
+
+    return (
+      <QRCodeScanner
+        onRead={onSuccess}
+        flashMode={RNCamera.Constants.FlashMode.torch}
+      />
+    );
 }
 
-export default QRView;
+const styles = StyleSheet.create({
+  centerText: {
+    flex: 1,
+    fontSize: 18,
+    padding: 32,
+    color: '#777'
+  },
+  textBold: {
+    fontWeight: '500',
+    color: '#000'
+  },
+  buttonText: {
+    fontSize: 21,
+    color: 'rgb(0,122,255)'
+  },
+  buttonTouchable: {
+    padding: 16
+  }
+});
+
+export default QrView;
